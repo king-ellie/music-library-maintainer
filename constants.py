5931b3df4932cecfce655278e634aacc0334432b
king-ellie <elliebking1@gmail.com>
2024-03-05 00:08:40 -0500

    create file with names of songs to download

--- constants.py
+++ constants.py
@@ -1,1 +1,6 @@
 ELLIE_USERNAME = 1219843066
+
+SG_FLAG = "SG:"
+
+PATH_TO_LOCAL_LIBRARY = "/Users/ellie/Music/Tracks"
+PATH_TO_DOWNLOAD_LOGS = "/Users/ellie/Music/Download_Logs"
--- script.py
+++ script.py
@@ -1,7 +1,10 @@
+from datetime import datetime
+from zoneinfo import ZoneInfo
 import spotipy
+import os
 from dotenv import load_dotenv
 from spotipy.oauth2 import SpotifyClientCredentials
-from constants import ELLIE_USERNAME
+from constants import ELLIE_USERNAME, SG_FLAG, PATH_TO_LOCAL_LIBRARY, PATH_TO_DOWNLOAD_LOGS
 
 load_dotenv()
 
@@ -10,9 +13,49 @@ sp = spotipy.Spotify(auth_manager=auth_manager)
 
 
 def main():
+    local_tracks = get_local_tracks(PATH_TO_LOCAL_LIBRARY)
+
     playlists = get_playlists(sp)
+    need_to_download_tracks = set()
+    playlist_to_new_tracks: dict[str, set] = {}
     for playlist in playlists:
-        print(f"Playlist: {playlist['name']}")
+        playlist_name = playlist.get("name", None)
+
+        if SG_FLAG in playlist_name:
+            print(f"Looking for new tracks in playlist: {playlist_name}")
+            try:
+                playlist_tracks = get_playlist_tracks(sp, playlist['id'])
+            except Exception as e:
+                print(f"Error retrieving tracks for playlist {playlist_name}: {e}")
+                continue
+
+            for track in playlist_tracks:
+                if track not in local_tracks:
+                    need_to_download_tracks.add(track)
+
+                    if not playlist_to_new_tracks.get(playlist_name, None):
+                        playlist_to_new_tracks[playlist_name] = set()
+                    playlist_to_new_tracks[playlist_name].add(track)
+
+    # Write tracks to text file
+    write_to_file(need_to_download_tracks)
+
+
+def write_to_file(tracks_list):
+    # Get today's date in Eastern Time
+    eastern = ZoneInfo("US/Eastern")
+    today_date = datetime.now(eastern).strftime("%m-%d-%y")
+
+    filename = f"tracks_to_download_{today_date}.txt"
+    filepath = os.path.join(PATH_TO_DOWNLOAD_LOGS, filename)
+
+    try:
+        with open(filepath, 'w') as file:
+            for track in tracks_list:
+                file.write(f"{track}\n")
+        print(f"List of tracks written to: {filepath}")
+    except Exception as e:
+        print(f"Error writing to file: {e}")
 
 def get_playlists(sp):
     playlist_resp = sp.user_playlists(ELLIE_USERNAME, limit=50)
@@ -24,6 +67,25 @@ def get_playlists(sp):
 
     return playlists
 
+def get_playlist_tracks(sp, playlist_id) -> list[str]:
+    tracks = []
+    results = sp.playlist_tracks(playlist_id)
+    tracks.extend([track['track']['name'] for track in results['items']])
+    
+    while results['next']:
+        results = sp.next(results)
+        tracks.extend([track['track']['name'] for track in results['items']])
+    
+    return tracks
+
+def get_local_tracks(path) -> list[str]:
+    local_tracks = []
+    for _, _, files in os.walk(path):
+        for file in files:
+            if file.endswith(".mp3"):
+                local_tracks.append(os.path.splitext(file)[0])
+    
+    return local_tracks
 
 if __name__ == "__main__":
     main()
@@ -31,3 +93,6 @@ if __name__ == "__main__":
 
 
 
+
+    # post playlist to spotify with need_to_download_list
+    # generate log file with hash dict
